94314fc16e8ee27c11a0dd0b1cf6855da3f222e8
S-Onix <msyoo101@hansung.ac.kr>
2024-01-30 22:24:11 +0900

    Config #1 - spring Configuration
    기초 설정
    패키지 이하 파일을 스프링이 관리하게 한다.

---
+++ src/main/java/org/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package org.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("org.springpractice")
+@Configuration
+public class Config {
+}

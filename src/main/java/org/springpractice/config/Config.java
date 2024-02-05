28f8d0c72b127d872dbb066bd3c8ab65a9381544
S-Onix <msyoo101@hansung.ac.kr>
2024-02-05 21:51:51 +0900

    #1 -spring apply

--- src/main/java/org/springpractice/config/Config.java
+++ src/main/java/org/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package org.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("org.springpractice")
 @Configuration
 public class Config {
